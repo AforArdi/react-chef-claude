009c80aae29b4bb8ee4edbdd5bb6cf190e6e2ec4
AforArdi <binsbro36@gmail.com>
2026-03-27 10:36:17 +0600

    conditional rendering added in main ingredients rendering part and also in the Ai triggering part in the main

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -32,10 +32,11 @@ const Main = () => {
 
     return ( 
         <div>
-            <div className="mx-w-2xl mx-atuo border mt-10">
+            <main className="max-w-2xl mx-auto w-full px-4 mt-8">
                 {/* onSubmit={handleSubmit} inside form */}
                 <form action={addIngredient} className="flex items-center justify-center gap-5">
-                    <input type="text" 
+                    <input 
+                    type="text" 
                     name="ingredient"
                     placeholder="e.g. oregano" 
                     className="input input-bordered join-item"
@@ -46,21 +47,29 @@ const Main = () => {
                         <FiPlusCircle></FiPlusCircle> Add ingredient
                     </button>
                 </form>
-            </div>
 
-            <div className="w-[11/12] mx-atuo border mt-10">
-                <h2 className="font-semibold text-2xl">Ingredients on hand:</h2>
-                <div className="mt-4">
-                    {
-                        ingredients.map((ingredient, i)=>{
-                            return <Ingredients
-                            key={i}
-                            ingredient={ingredient}
-                            ></Ingredients>
-                        })
-                    }
-                </div>
-            </div>
+                {ingredients.length > 0 && <section className="w-full mt-8">
+                    <h2 className="font-semibold text-2xl">Ingredients on hand:</h2>
+                    <div className="mt-4">
+                        {
+                            ingredients.map((ingredient, i)=>{
+                                return <Ingredients
+                                key={i}
+                                ingredient={ingredient}
+                                ></Ingredients>
+                            })
+                        }
+                    </div>
+                    {ingredients.length > 3 && <div className="flex justify-between items-center bg-[#6b728028] p-6 rounded-md mt-6">
+                        <div>
+                            <p className="text-[20px] font-semibold">Ready for a recipe?</p>
+                            <p className="text-[16px]">Generate a recipe from your list of ingredients.</p>
+                        </div>
+                        <button className="btn bg-[#D17557] text-white">Get a recipe</button>
+                    </div>}
+                </section>}
+            </main>
+            
         </div>
      );
 }
