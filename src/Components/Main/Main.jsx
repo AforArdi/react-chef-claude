0f8ec22ed28d3276832c9f0aebbb9ed792e011c4
AforArdi <binsbro36@gmail.com>
2026-03-27 07:22:26 +0600

    handling the form with latest react way

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import React from "react";
 import { FiPlusCircle } from "react-icons/fi";
 import Ingredients from "./Ingredients/Ingredients";
 
@@ -6,23 +7,39 @@ const Main = () => {
     const [inputValue, setInputValue] = useState('');
     const [ingredients, setIngredients] = useState([]);
 
+    // this function below is for onDubmit
     const handleSubmit=(e)=>{
         e.preventDefault();
         if(inputValue.trim() === ''){
             return;
         }
         setIngredients([...ingredients, inputValue]);
+        // setIngredients(prev => [...prev, inputValue]); this is more pro.
         setInputValue('');
+    }
+
+    // this function below is for action attribute
+    // currently active fucntion
+    const addIngredient=(formData)=>{
+        const newIngredient = formData.get('ingredient')
+        if(newIngredient.trim() === ''){
+            return;
+        }
+
+        setIngredients(prevIngredients=> [...prevIngredients, newIngredient]);
+        // setInputValue('');
     }
 
     return ( 
         <div>
             <div className="mx-w-2xl mx-atuo border mt-10">
-                <form onSubmit={handleSubmit} action="" className="flex items-center justify-center gap-5">
+                {/* onSubmit={handleSubmit} inside form */}
+                <form action={addIngredient} className="flex items-center justify-center gap-5">
                     <input type="text" 
+                    name="ingredient"
                     placeholder="e.g. oregano" 
                     className="input input-bordered join-item"
-                    value={inputValue}
+                    // value={inputValue} don't need this when using the action, and formData method
                     onChange={(e)=>setInputValue(e.target.value)}
                     />
                     <button type="submit" className="btn btn-neutral flex items-center gap-1">
